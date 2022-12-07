dbb2f4383782ff4d7c2ecb85e07e6848e706c64c
Lucie Le Frapper <lucielefrapper@MacBook-Pro-de-Lucie.local>
2022-12-07 15:05:55 +0100

    add day5 part 1

---
+++ day5/05.01.py
@@ -0,0 +1,53 @@
+import re 
+
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
+    #lines = [line.rstrip() for line in lines] Cette ligne fausse le nombre d'espace en bout de ligne
+
+columns = 9 #Dans l'idéal cette valeur devrait être calculée... 
+columnsList = [[] for i in range(columns)]
+instruction = []
+stacks = []
+
+for line in lines: 
+
+    if line == '\n':
+        break
+    for i, j in zip(range(1, columns * 4 - 1, 4), range(columns)): #Itère à la fois sur les caractères de la ligne et sur les tableaux 
+        if line[i] != " " and ord(line[i]) > 57: #Supprime les caractères numériques en se basant sur la valeur ASCII
+            columnsList[j].append(line[i])
+
+
+for line in lines: 
+    instruction.append(line)
+
+    if line.strip() == "":
+        instruction = []
+
+moveorder = []
+for line in instruction:
+    moveorder.append([int(s) for s in re.findall(r'\b\d+\b', line)])
+   
+    
+for tab in moveorder:
+    numberofcrates = tab[0]
+    departstack = tab[1]
+    arrivalstack = tab[2]
+    
+    cratestomove = columnsList[departstack -1][0:numberofcrates] #On ajoute -1 car la liste des colonnes démarre à 0
+    print("les caisses à bouger", cratestomove)
+    
+    for i in range(len(cratestomove)):
+        print("La colonne de départ", columnsList[departstack -1])
+        print("la colonne d'arrivée", columnsList[arrivalstack -1])
+        columnsList[departstack -1].remove(cratestomove[i])
+        print("La colonne de départ après avoir enlever les caisses", columnsList[departstack -1])
+        columnsList[arrivalstack -1][:0] = cratestomove[i]
+        print("La colonne d'arrivée après avoir ajouté les caisses", columnsList[arrivalstack -1] )
+    
+
+def Extract(lst):
+    return [item[0] for item in lst]
+
+print(''.join(Extract(columnsList)))
+
